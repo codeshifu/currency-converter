915228a3edaa65351481302cbd9fcdc2dbe21d7e
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-30 17:49:00 +0100

    handle promise rejection gracefully

--- js/requests.js
+++ js/requests.js
@@ -3,12 +3,18 @@
   window.$convert = (query, cb) => {
     fetch(`${BASE_URL}/convert?q=${query}&compact=ultra`)
       .then(res => res.json())
-      .then(result => cb(result[query]));
+      .then(result => cb(result[query]))
+      .catch(err =>
+        console.log(
+          'Unable to do conversion. Pls check your internet connection or try again later.'
+        )
+      );
   };
 
   window.$getCurrencies = cb => {
     fetch('/currencies.json')
       .then(res => res.json())
-      .then(currencies => cb(currencies.results));
+      .then(currencies => cb(currencies.results))
+      .catch(err => console.log('Error fetching currencies.'));
   };
 })(window);
