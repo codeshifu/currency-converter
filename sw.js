a932dfc551d96fa4acde046ceaab80b45d86cb87
Olushi Luqman O <olushilukmon03@gmail.com>
2018-07-01 21:01:03 +0100

    hijack requests to freecurrencyconverter api - respond with cache then update cache with latest data from api

--- sw.js
+++ sw.js
@@ -44,9 +44,23 @@ self.onfetch = e => {
 
   if (requestURL.hostname === location.hostname) {
     e.respondWith(cacheOrNetwork(requestURL));
+  } else if (requestURL.hostname === 'free.currencyconverterapi.com') {
+    e.respondWith(cacheOrNetwork(requestURL));
+
+    e.waitUntil(updateCache(requestURL));
   }
 };
 
 const cacheOrNetwork = request => {
-  return caches.match(request).then(response => response || fetch(request));
+  return caches
+    .match(request)
+    .then(response => response || fetch(request).catch(() => null));
+};
+
+const updateCache = request => {
+  return caches.open(freeCurrConverterAPI).then(cache =>
+    fetch(request)
+      .then(response => cache.put(request, response))
+      .catch(() => null)
+  );
 };
